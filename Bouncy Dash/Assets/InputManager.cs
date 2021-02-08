1af66c3abbd2626c36e22b995bc1ab971a5ff962
Short1398 <short1398@gmail.com>
2021-02-08 17:26:32 -0500

    Ready to solve my merge conflict yay

--- Bouncy Dash/Assets/InputManager.cs
+++ Bouncy Dash/Assets/InputManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     const string HORIZONTALMOV = "Horizontal";
-    const string JUMP_BUTTON = "Vertical";
+    const string JUMP_BUTTON = "CustomJump";
 
     public static Vector2 GetMovementInput()
     {
