0cd501ef9ccb39d230805e4b894132edabe7197b
Short1398 <short1398@gmail.com>
2021-02-26 13:13:45 -0500

    Hornet AI implemented
    Hornet can move to assigned target waypoints and will stop to turn around and lookout as often as desired

--- Bouncy Dash/Assets/Scripts/Components/Enemy_Base.cs
+++ Bouncy Dash/Assets/Scripts/Components/Enemy_Base.cs
@@ -7,14 +7,30 @@ public class Enemy_Base : MonoBehaviour
     protected enum EnemyTypes
     {
         TURRET, 
-        GRUNT
+        GRUNT,
+        FLYING
     }
 
     [SerializeField]
     protected EnemyTypes m_enemyType;
     [Header("Damage")]
     [SerializeField]
-    protected float damageDealt;
+    protected float m_damageDealt;
 
+    [Header("Mobility")]
+    [SerializeField]
+    protected float m_accTime;
+    [SerializeField]
+    protected float m_decTime;
+    [SerializeField]
+    protected float m_maxHorizontalSpeed;
+    [SerializeField]
+    protected float m_maxVerticalSpeed;
+
+    //Movement tracking 
+    protected float m_currentHorizontalSpeed;
+    protected float m_currentVerticalSpeed;
+
+    //Find player
     protected const string PLAYER_TAG = "Player";
 }
