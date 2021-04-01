9b5673e47922b6d94352e0eb39dce1fa2e0837c2
Eric Vander Horst <eric.vh1999@gmail.com>
2021-03-31 20:18:19 -0400

    Finish level end gate basic functionality, 1st gate is at bottom of tunnel

--- Bouncy Dash/Assets/Scripts/LevelEndGate.cs
+++ Bouncy Dash/Assets/Scripts/LevelEndGate.cs
@@ -11,7 +11,8 @@ public class LevelEndGate : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(SceneManager.GetActiveScene().buildIndex + 1).name);
+            //I copied this over
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
