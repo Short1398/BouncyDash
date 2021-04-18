11366f8d79382d13f0ca5160a6c897228edddb5e
Chris <crhewson@gmail.com>
2021-04-17 20:33:34 -0400

    -Scaled down charge particles -added a basic pause menu that lists controls

---
+++ Bouncy Dash/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Basic functionality for pausing
+
+public class PauseGame : MonoBehaviour {
+
+    public GameObject PausePanel;
+    bool isPaused = false;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused) {
+            Time.timeScale = 0;
+            PausePanel.SetActive(true);
+        } else if (isPaused == false) {
+            Time.timeScale = 1;
+            PausePanel.SetActive(false);
+        }
+    }
+}
